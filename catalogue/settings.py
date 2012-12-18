2e13fa34437f0a9d1388c3acff51e7876df511b1
Olivier Aubert <contact@olivieraubert.net>
2012-12-18 21:11:57 +0100

    Add a / to dir name

--- catalogue/settings.py
+++ catalogue/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = 'http://localhost:8000/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = APPROOT + 'static'
+STATIC_ROOT = APPROOT + 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
