ee499e50225b174210c95d3f37c8052ec4442cc5
Olivier Aubert <contact@olivieraubert.net>
2012-12-20 22:44:21 +0100

    Add /media URL handling

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^base/', include('base.urls')),
                        url(r'^admin/', include(admin.site.urls)),
-                       (r'^', include('coop_tag.urls')),
+                       url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+                       url(r'^', include('coop_tag.urls')),
 )
