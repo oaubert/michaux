514e4b5b4aca4f501056ff1c9f175377f62262f3
Olivier Aubert <contact@olivieraubert.net>
2012-12-21 10:07:34 +0100

    Add redirection from app root to /work

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url, include
 #from rest_framework.urlpatterns import format_suffix_patterns
-#from tmg import views
 
 urlpatterns = patterns('',
                        url(r'^$', 'base.views.root'),
+                       url(r'^work/$', 'base.views.root'),
                        url(r'^work/(?P<cote>\d+)/$', 'base.views.work'),
                        )
