49cf6bf712ed777234b22a91933a5c4ab118c80c
Olivier Aubert <contact@olivieraubert.net>
2013-01-11 22:35:30 +0100

    Implement templatetags autolink and url_remove_tag

---
+++ base/templatetags/michaux.py
@@ -0,0 +1,25 @@
+import re
+import urllib
+
+from django.template.defaultfilters import stringfilter
+from django import template
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def autolink(source):
+    """Automatically decorate certain strings with links:
+    #NNN -> link to work
+    facet value -> link to facetted search
+    """
+    # FIXME: use correct reverse function
+    source = re.sub('#(\d+)', r'<a href="/base/work/\g<1>">\g<0></a>', source)
+    return source
+
+@register.filter
+@stringfilter
+def url_remove_facet(url, facet_value):
+    """
+    """
+    return re.sub('(f=\w+:%s(&|$))' % urllib.quote(facet_value[0]), "", url)
