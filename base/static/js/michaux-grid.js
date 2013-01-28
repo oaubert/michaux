e2c8813392202e84a6e0dd88a620d686f68e04f6
Olivier Aubert <contact@olivieraubert.net>
2013-01-28 22:32:18 +0100

    Replace the lightbox component by a sliding panel

--- base/static/js/michaux-grid.js
+++ base/static/js/michaux-grid.js
@@ -117,36 +117,11 @@ jQuery(document).ready(
                                  //Get clicked link href
                                  var image_href = $(this).attr("href");
                                  var cote = $(this).parents("div.work").attr('data-cote');
-                                 /*
-                                  If the lightbox window HTML already exists in document,
-                                  change the img src to to match the href of whatever link was clicked
-                                  If the lightbox window HTML doesn't exists, create it and insert it.
-                                  (This will only happen the first time around)
-                                  */
-                                 if ($('#lightbox').length > 0) { // #lightbox exists
-                                     //place href as img src value
-                                     $('#lightbox_content').html('<img src="' + image_href + '" />');
-                                     //show lightbox window - you could use .show('fast') for a transition
-                                     $('#lightbox').show('fast');
-                                 }
-                                 else { //#lightbox does not exist - create and insert (runs 1st time only)
-                                     //create HTML markup for lightbox window
-                                     var lightbox =
-                                         '<div id="lightbox">' +
-                                         '<p>Click to close</p>' +
-                                         '<div id="lightbox_info"></div>' +
-                                         '<div id="lightbox_content">' + //insert clicked link's href into img src
-                                         '<img src="' + image_href +'" />' +
-                                         '</div>' +
-                                         '</div>';
-                                     //insert lightbox HTML into page
-                                     $('body').append(lightbox);
-                                 }
                                  // AJAX callback to insert the lightbox_info
                                  // FIXME: hardcoded URL. Should fix this.
-                                 console.log('/base/work/' + cote + '/info');
                                  $.get(cote + '/info', function (data) {
-                                           $('#lightbox_info').html(data);
+                                           $('#info_panel').html(data);
+                                           $('#info_panel').show('fast');
                                        });
                              });
         //Click anywhere on the page to get rid of lightbox window
