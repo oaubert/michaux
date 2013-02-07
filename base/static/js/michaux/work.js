09a2a5cc2d7eb3227db2bf1bce177cec838cc4fc
Olivier Aubert <contact@olivieraubert.net>
2013-02-08 00:07:34 +0100

    Add context display in thumbnail for wheelzoom image

--- base/static/js/michaux/work.js
+++ base/static/js/michaux/work.js
@@ -6,8 +6,35 @@ jQuery(document).ready(
         // Make tags clickable
         $(".as-selection-item").contents(':not(a)').click( function () { document.location = "/base/work?f=tags_exact:" + encodeURIComponent($(this).text()); });
 
+        function draw_frame(vignette) {
+            var rubber = $(vignette).find(".rubber_band");
+            var thumbnail = $(vignette).find("img");
+            var width = 1.0 * $(thumbnail).width();
+            var height = 1.0 * $(thumbnail).height();
+            // Generate a callback function to display a zoomed rectangle over the thumbnail
+            return function(x, y, w, h) {
+                rubber.css({
+                               left: Math.floor(x * width) + 'px',
+                               top: Math.floor(y * height) + 'px',
+                               width: Math.floor(w * width) + 'px',
+                               height: Math.floor(h * height) + 'px',
+                               display: 'block'
+                           });
+                return false;
+            };
+        }
+
         // Add wheelzoom to image
-        $(".display").wheelzoom();
+        $("[rel=lightbox]").append($("<div/>")
+                                   .addClass('rubber_band')
+                                   .css({
+                                            position: "absolute",
+                                            display: "none",
+                                            width: "0px",
+                                            height: "0px",
+                                            border: "2px solid red"
+                                        }));
+        $(".display").wheelzoom({callback: draw_frame($("[rel=lightbox]"))});
 
         michaux.tag_selection = function (tagname) {
             var cote = $(".workinfo").attr('data-cote');
