8769e4dfe7ca538f42799c914e43f1b48c532b5a
Olivier Aubert <contact@olivieraubert.net>
2013-03-12 12:31:49 +0100

    Admin: allow to show/hide changelist filter by clicking on its title

--- base/static/js/michaux/admin.js
+++ base/static/js/michaux/admin.js
@@ -3,10 +3,16 @@ var michaux = {};
 
 jQuery(document).ready(
     function($) {
+        document.$ = $;
         $("[name$=technique]").autocomplete({ source: "/base/complete/technique",
                                               minLength: 2});
         $("[name$=support]").autocomplete({ source: "/base/complete/support",
                                             minLength: 2});
         $("[name$=serie]").autocomplete({ source: "/base/complete/serie",
                                           minLength: 2});
-    });
+        $("#changelist-filter h2").click( function() { $("#changelist-filter").children("h3,ul").each(
+                                                           function () {
+                                                               $(this).toggleClass("closed");
+                                                           });
+                                                     });
+});
