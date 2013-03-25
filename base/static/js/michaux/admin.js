0bfda4096e7599ca2f56f33cc9e818aa615ef141
Olivier Aubert <contact@olivieraubert.net>
2013-03-25 16:23:48 +0100

    Transform help text into tooltips

--- base/static/js/michaux/admin.js
+++ base/static/js/michaux/admin.js
@@ -7,6 +7,12 @@ django.jQuery(document).ready(
         // Move navbar to header
         $("ul.navbar").insertBefore("#grp-admin-title");
 
+        // Transform help text into tooltips
+        $(".grp-help").each( function () {
+                                 $(this).siblings("input,select,textarea").attr("title", $(this).text());
+                             })
+            .hide();
+
         completable_fields.forEach(function(s) {
                                        $("[name$=" + s + "]").autocomplete({ source: "/base/complete/" + s,
                                                                              minLength: 2});
