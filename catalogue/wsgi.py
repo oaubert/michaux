058014599ab4a689b6f4fae74efb84ca91eef0cd
Olivier Aubert <contact@olivieraubert.net>
2013-05-22 17:07:56 +0200

    Set pythonpath for wsgi server

--- catalogue/wsgi.py
+++ catalogue/wsgi.py
@@ -14,8 +14,12 @@ framework.
 
 """
 import os
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "catalogue.settings")
+
+d = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(d))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
