278c445fe53d475df4557386f5d16386796cc5e2
Olivier Aubert <contact@olivieraubert.net>
2013-12-11 22:20:29 +0100

    Implement ventes_publiques import

--- base/management/commands/michaux.py
+++ base/management/commands/michaux.py
@@ -8,13 +8,14 @@ import datetime
 from django.core.files import File
 from django.core.management.base import BaseCommand, CommandError
 
-from base.models import Work, Image, Inscription, Exhibition, BibliographyReference, ExhibitionInstance
+from base.models import Work, Image, Inscription, Exhibition, BibliographyReference, ExhibitionInstance, Acquisition
 
 class Command(BaseCommand):
     args = '[command] [param]'
     help = """Administration commands for Michaux catalogue
   works <xls file> : import the Michaux catalogue from an xls file
   exhibitions <xls file> : import the Michaux exhibition list
+  ventes <xls files> : import the ventes publiques list
   gen_abbreviations : generate standard abbreviations for exhibitions
 """
     def _import_works_from_xls(self, filename):
@@ -109,6 +110,158 @@ class Command(BaseCommand):
             if data['Expositions'].strip():
                 notfound = []
                 for ab in data['Expositions'].split('\n'):
+                    ab = ab.strip().strip(",")
+                    if not ab:
+                        # Blank line
+                        continue
+                    qs = Exhibition.objects.filter(abbreviation=ab)
+                    if qs.count() == 1:
+                        ex = qs[0]
+                        # Found the exhibition
+                        ei = ExhibitionInstance()
+                        ei.work = w
+                        ei.exhibition = ex
+                        ei.save()
+                        # Associate also "reprises"
+                        for reprise in Exhibition.objects.filter(original=ex):
+                            ei = ExhibitionInstance()
+                            ei.work = w
+                            ei.exhibition = reprise
+                            ei.save()
+                        msg = 'ok'
+                    else:
+                        msg = 'NOT FOUND'
+                        notfound.append(ab)
+                    self.stderr.write("   Exhibition %s... %s\n" % (ab.encode('utf-8'), msg))
+                    if notfound:
+                        w.revision += u"\n".join(u"EXPO: %s" % e for e in notfound if e)
+                        w.save()
+
+    def _import_ventes_from_xls(self, filename):
+        """Import from an xls sellings file.
+        """
+        self.stdout.write("** Importing ventes\n")
+        import xlrd
+        book = xlrd.open_workbook(filename)
+        s = book.sheet_by_index(0)
+        header = s.row_values(0)
+        # côte	certificat	technique	précisions sur la technique	série	année	dimensions	support	support 1	support 2	support 3	signature	notes de michaux	notice	remarques	expositions	reproductions	propriétaire	image	date	lieu	catalogue	estimation	prix de vente
+
+        for n in range(1, s.nrows - 1):
+            #if n < 80:
+            #    continue
+            #if n > 600:
+            #    break
+            row = s.row_values(n)
+            data = dict(zip(header, row))
+            w = Work()
+            # FIXME: get appropriate value
+            w.creator_id = 1
+            w.contributor_id = 1
+            w.status = Work.AUTHENTICATED_STATUS
+            w.old_references = data[u'côte']
+            if data['certificat'] != '':
+                cert = re.findall('(\d+)', data['certificat'])
+                if cert:
+                    w.certificate = long(cert[0])
+            # Remove whitespaces around commas
+            w.serie = data[u'série']
+            w.technique = ",".join(re.split('\s*,\s*', data['technique'].strip()))
+            w.note_technique = data[u'précisions sur la technique']
+            w.support = data['support 1']
+            w.support_details = data['support 2']
+            w.note_support = data['support 3']
+            try:
+                (l, h) = re.split('\s*x\s*', data['dimensions'])
+                l = int(10 * float(l.replace(',', '.')))
+                h = int(10 * float(h.replace(',', '.')))
+                w.height = h
+                w.width = l
+            except ValueError:
+                self.stderr.write("Missing height/width for %s\n" % data[u'côte'].encode('utf-8'))
+                w.height = 0
+                w.width = 0
+
+            annee = re.findall('19(\d\d)', data[u'année'])
+            if annee:
+                w.creation_date_start = long(annee[0])
+            if annee == [] or unicode(long(annee[0])) != data[u'année'].strip():
+                w.note_creation_date = data[u'année']
+
+            w.note = data['notice']
+            w.comment = data['remarques']
+            if data[u'reproductions']:
+                w.revision = u"\n".join(u"BIBLIO: %s" % b for b in data[u'reproductions'].splitlines() if b.strip())
+            w.save()
+            self.stderr.write("Saved %s %s\n" % (n, unicode(w).encode('utf-8')))
+
+            # FIXME: Improve image name heuristic
+            if data[u'image']:
+                pic = '/home/oaubert/tmp/sdv/%s' % data[u'image'].lower()
+                if os.path.exists(pic.encode('utf-8')):
+                    self.stderr.write("   Copying image %s\n" % pic.encode('utf-8'))
+                    i = Image()
+                    i.work = w
+                    i.photograph_name = 'Franck Leibovici'
+                    i.support = u'numérique'
+                    i.nature = u'référence'
+                    with open(pic, 'rb') as f:
+                        i.original_image.save(os.path.basename(pic), File(f))
+                    i.save()
+
+            if data[u'signature'].startswith('oui'):
+                sig = Inscription()
+                sig.nature = 'signature'
+                pos = re.findall('\((.+)\)', data[u'signature'])
+                if pos:
+                    if ',' in pos[0]:
+                        sig.position, sig.note = re.split('\s*,\s*', pos[0], 1)
+                    else:
+                        sig.position = pos[0]
+                sig.work = w
+                sig.save()
+
+            note = data[u'notes de michaux']
+            if note:
+                sig = Inscription()
+                sig.nature = 'note'
+                pos = re.findall('\(([hbcgd]{2})\)', note)
+                if pos:
+                    sig.position = pos[0]
+                sig.note = note
+                sig.work = w
+                sig.save()
+
+            # Process Acquisitions
+            # propriétaire	date	lieu	catalogue	estimation	prix de vente
+            if data['lieu'].strip():
+                acq = Acquisition()
+                acq.location = data['lieu'].strip()
+
+                if data['date']:
+                    try:
+                        d = datetime.date.fromordinal(long(data['date']))
+                        if d.year < 1900 and d.year > 0:
+                            # Add 1900
+                            d=datetime.date(1900 + d.year, d.month, d.day)
+                        # Strange: there is a 1-year offset
+                        acq.year = d.year - 1
+                        acq.month = d.month
+                        acq.day = d.day
+                    except ValueError:
+                        print "Error when parsing date"
+                        pass
+                acq.reference = data['catalogue'].strip()
+                acq.estimation = data['estimation'].strip()
+                if data['prix de vente']:
+                    try:
+                        acq.price = long(data['prix de vente'])
+                    except ValueError:
+                        pass
+
+            if data['expositions'].strip():
+                notfound = []
+                for ab in data['expositions'].split(';'):
                     ab = ab.strip().strip(",")
                     if not ab:
                         # Blank line
@@ -298,7 +451,7 @@ class Command(BaseCommand):
             ex.abbreviation = ab
             ex.save()
         # FIXME: migrate abbreviations to BibliographyReference table
-                
+
     def handle(self, *args, **options):
         if not args:
             self.print_help(sys.argv[0], sys.argv[1])
@@ -309,6 +462,7 @@ class Command(BaseCommand):
             'works': self._import_works_from_xls,
             'exhibitions': self._import_exhibitions_from_xls,
             'gen_abbreviations': self._generate_abbreviations,
+            'ventes': self._import_ventes_from_xls,
             }
         m = dispatcher.get(command)
         if m is not None:
