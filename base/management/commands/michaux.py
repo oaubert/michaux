640d79963dfe3ec6362a0079e6cec8b83770bd7b
Olivier Aubert <contact@olivieraubert.net>
2014-10-04 23:17:57 +0200

    Check only references with associated filename

--- base/management/commands/michaux.py
+++ base/management/commands/michaux.py
@@ -569,6 +569,8 @@ class Command(BaseCommand):
             header = r.next()
             for l in r:
                 ref, i, name = l
+                if not name.strip():
+                    continue
                 self.stderr.write(u"Checking %s" % ref)
                 try:
                     s = "MISSINGIMAGE: %s.png" % name
